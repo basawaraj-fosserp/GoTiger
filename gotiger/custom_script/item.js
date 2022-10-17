c1d5c7d2d1024d3293a38718d375505b1d6f7349
tailorraj <tailorraj111@gmai.com>
2022-10-17 07:05:10 +0000

    custom doctype

--- gotiger/custom_script/item.js
+++ gotiger/custom_script/item.js
@@ -18,21 +18,13 @@ frappe.ui.form.on('Item', {
                         }
                     }
                     cur_frm.refresh_field("uoms");
-	    } 
-        cur_frm.set_value('case_volume_in_l',flt(cur_frm.doc.case_dimension_length*cur_frm.doc.case_dimension_width*cur_frm.doc.case_dimension_height)/1000)  
-        cur_frm.refresh_field('case_volume_in_l') 
-        if(cur_frm.doc.units_per_case != 0){
-            cur_frm.set_value('unit_volume_in_l',flt(cur_frm.doc.case_volume_in_l/cur_frm.doc.units_per_case))  
-            cur_frm.refresh_field('unit_volume_in_l')
-        }
-        
+	    }      
 	},
 	after_save(cur_frm){
         cur_frm.doc.uoms.forEach(u =>{
             if(u.uom =="Case"){
-                u.conversion_factor = frm.doc.units_per_case;
+                u.conversion_factor = cur_frm.doc.units_per_case;
             }
-            cur_frm.refresh_field("uoms");
         }); 
         
         
