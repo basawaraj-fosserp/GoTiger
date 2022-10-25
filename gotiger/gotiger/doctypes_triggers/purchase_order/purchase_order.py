ad4eaecf778a023bd55c0632667fb6a69e899266
basawaraj-fosserp <103717083+basawaraj-fosserp@users.noreply.github.com>
2022-10-25 21:54:47 +0530

    Revised Code for Appending Items

--- gotiger/gotiger/doctypes_triggers/purchase_order/purchase_order.py
+++ gotiger/gotiger/doctypes_triggers/purchase_order/purchase_order.py
@@ -1,77 +1,80 @@
-from unicodedata import name
+
 import frappe
 
 @frappe.whitelist()
-def append_items(supplier,name):
-    
-    # doc = json.loads(doc)
-    item_doc = frappe.get_doc('Item Purchase Plan', {'supplier':supplier })
-    # frappe.msgprint(str(item_doc))
-    # frappe.msgprint(str(item_doc.item))
-    
-    purchase_doc = frappe.get_doc('Purchase Order',{'name':name})
-    # frappe.msgprint(str(purchase_doc.supplier))
+def append_items(supplier, name):
+    # Get the actual PO
+    doc = frappe.get_doc('Purchase Order', name)
 
-    qty_value = frappe.db.sql("""
-        select
-        po.supplier,
-        poi.item_code,
-        poi.qty, 
-        poi.received_qty,
-        (poi.qty - poi.received_qty) as remain_qty
-        from
-        `tabPurchase Order` po,
-        `tabPurchase Order Item` poi
-        LEFT JOIN `tabPurchase Invoice Item` pii ON pii.po_detail = poi.name
-        where
-        poi.parent = po.name
-        and po.docstatus = 1 and po.supplier = %s and poi.item_code = %s """,
-        (purchase_doc.supplier,item_doc.item),as_dict = 1)	   
-    # frappe.msgprint(str(qty_value[0]['remain_qty']))    
-    
-    if purchase_doc.supplier == item_doc.supplier:
-       for item in purchase_doc.items: 
-        if item.item_code == item_doc.item:
-            uom_value = frappe.get_doc('Item',{'item_code':item_doc.item})
-            # frappe.msgprint(str(uom_value.uoms))
-            for u in uom_value.uoms:
-                # frappe.msgprint(u.uom)
-                if u.uom == "Case":
-                    item.uom = u.uom
-                    item.conversion_factor = uom_value.units_per_case
-                    item_purchase_calculation = (((item_doc.sales_velocity*item_doc.lead_time_in_days)+(item_doc.sales_velocity*item_doc.coverage_days)) - (qty_value[0]['remain_qty'] + frappe.db.get_value('Bin',{'warehouse':item_doc.warehouse,'item_code':item_doc.item},'actual_qty')-((item_doc.sales_velocity*item_doc.lead_time_in_days)-(item_doc.sales_velocity*item_doc.coverage_days))))/uom_value.units_per_case
-                    frappe.msgprint(str(item_purchase_calculation))
-                    if item_purchase_calculation > 0:
-                    # item.qty = qty_value[0]['remain_qty'] + frappe.db.get_value('Bin',{'warehouse':item_doc.warehouse,'item_code':item_doc.item},'actual_qty')-((item_doc.sales_velocity*item_doc.lead_time_in_days)-(item_doc.sales_velocity*item_doc.coverage_days))
-                        item.qty = round((((item_doc.sales_velocity*item_doc.lead_time_in_days)+(item_doc.sales_velocity*item_doc.coverage_days)) - (qty_value[0]['remain_qty'] + frappe.db.get_value('Bin',{'warehouse':item_doc.warehouse,'item_code':item_doc.item},'actual_qty')-((item_doc.sales_velocity*item_doc.lead_time_in_days)-(item_doc.sales_velocity*item_doc.coverage_days))))/uom_value.units_per_case)
-                        item.rate = frappe.db.get_value('Item Price',{'price_list':purchase_doc.buying_price_list,'item_code':item_doc.item},'price_list_rate')
-                        item.price_list_rate = frappe.db.get_value('Item Price',{'price_list':purchase_doc.buying_price_list,'item_code':item_doc.item},'price_list_rate')
-                        item.base_price_list_rate = frappe.db.get_value('Item Price',{'price_list':purchase_doc.buying_price_list,'item_code':item_doc.item},'price_list_rate')
-                        # item.amount = qty_value[0]['remain_qty'] + frappe.db.get_value('Bin',{'warehouse':item_doc.warehouse,'item_code':item_doc.item},'actual_qty')-((item_doc.sales_velocity*item_doc.lead_time_in_days)-(item_doc.sales_velocity*item_doc.coverage_days))*frappe.db.get_value('Item Price',{'price_list':purchase_doc.buying_price_list,'item_code':item_doc.item},'price_list_rate')
-                        #item.amount = round((((item_doc.sales_velocity*item_doc.lead_time_in_days)+(item_doc.sales_velocity*item_doc.coverage_days)) - (qty_value[0]['remain_qty'] + frappe.db.get_value('Bin',{'warehouse':item_doc.warehouse,'item_code':item_doc.item},'actual_qty')-((item_doc.sales_velocity*item_doc.lead_time_in_days)-(item_doc.sales_velocity*item_doc.coverage_days))))/uom_value.units_per_case*frappe.db.get_value('Item Price',{'price_list':purchase_doc.buying_price_list,'item_code':item_doc.item},'price_list_rate'))
-                    else:
-                        frappe.throw("Qty value Can Not Be Negative")    
-                else:
-                    #qty_value[0]['remain_qty'] + frappe.db.get_value('Bin',{'warehouse':item_doc.warehouse,'item_code':item_doc.item},'actual_qty')
-                    value_qty = qty_value[0]['remain_qty'] if ((not qty_value[0]['remain_qty'] is None) and (len(qty_value[0]['remain_qty']) > 0)) else 0
-                    # item.qty = qty_value[0]['remain_qty'] + frappe.db.get_value('Bin',{'warehouse':item_doc.warehouse,'item_code':item_doc.item},'actual_qty')-((item_doc.sales_velocity*item_doc.lead_time_in_days)-(item_doc.sales_velocity*item_doc.coverage_days))
-                    item.qty = ((item_doc.sales_velocity*item_doc.lead_time_in_days)+(item_doc.sales_velocity*item_doc.coverage_days)) - (value_qty + frappe.db.get_value('Bin',{'warehouse':item_doc.warehouse,'item_code':item_doc.item},'actual_qty')-((item_doc.sales_velocity*item_doc.lead_time_in_days)-(item_doc.sales_velocity*item_doc.coverage_days)))
-                    item.rate = frappe.db.get_value('Item Price',{'price_list':purchase_doc.buying_price_list,'item_code':item_doc.item},'price_list_rate')
-                    item.price_list_rate = frappe.db.get_value('Item Price',{'price_list':purchase_doc.buying_price_list,'item_code':item_doc.item},'price_list_rate')
-                    item.base_price_list_rate = frappe.db.get_value('Item Price',{'price_list':purchase_doc.buying_price_list,'item_code':item_doc.item},'price_list_rate')
-                    # item.amount = qty_value[0]['remain_qty'] + frappe.db.get_value('Bin',{'warehouse':item_doc.warehouse,'item_code':item_doc.item},'actual_qty')-((item_doc.sales_velocity*item_doc.lead_time_in_days)-(item_doc.sales_velocity*item_doc.coverage_days))*frappe.db.get_value('Item Price',{'price_list':purchase_doc.buying_price_list,'item_code':item_doc.item},'price_list_rate')
-                    #item.amount = ((item_doc.sales_velocity*item_doc.lead_time_in_days)+(item_doc.sales_velocity*item_doc.coverage_days)) - (qty_value[0]['remain_qty'] + frappe.db.get_value('Bin',{'warehouse':item_doc.warehouse,'item_code':item_doc.item},'actual_qty')-((item_doc.sales_velocity*item_doc.lead_time_in_days)-(item_doc.sales_velocity*item_doc.coverage_days)))*frappe.db.get_value('Item Price',{'price_list':purchase_doc.buying_price_list,'item_code':item_doc.item},'price_list_rate')
+    # Get the Plans filtered by supplier and warehouse
+    plans = frappe.get_list('Item Purchase Plan', filters={
+        'supplier': supplier,
+        'warehouse': doc.set_warehouse
+    }, fields='*')
 
-            # purchase_doc.append("items",{
-            #     'item_code':item_doc.item,
-            #     'item_name':item_doc.item_name,
-            #     'qty':qty_value[0]['remain_qty'] + frappe.db.get_value('Bin',{'warehouse':item_doc.warehouse,'item_code':item_doc.item},'actual_qty')-((item_doc.sales_velocity*item_doc.lead_time_in_days)-(item_doc.sales_velocity*item_doc.coverage_days)),
-            #     'price_list_rate':frappe.db.get_value('Item Price',{'price_list':purchase_doc.buying_price_list,'item_code':item_doc.item},'price_list_rate'),
-            #     'base_price_list_rate':frappe.db.get_value('Item Price',{'price_list':purchase_doc.buying_price_list,'item_code':item_doc.item},'price_list_rate'),
-            #     'rate': frappe.db.get_value('Item Price',{'price_list':purchase_doc.buying_price_list,'item_code':item_doc.item},'price_list_rate'),
-            #     'amount':qty_value[0]['remain_qty'] + frappe.db.get_value('Bin',{'warehouse':item_doc.warehouse,'item_code':item_doc.item},'actual_qty')-((item_doc.sales_velocity*item_doc.lead_time_in_days)-(item_doc.sales_velocity*item_doc.coverage_days))*frappe.db.get_value('Item Price',{'price_list':purchase_doc.buying_price_list,'item_code':item_doc.item},'price_list_rate')
-            # })
-  
-    purchase_doc.save()  
-           
-   
+    # Iterate trought the plans
+    for plan in plans:
+        # Fetch the purchase_uom and stock_uom
+        purchase_uom, stock_uom = frappe.db.get_value('Item', {
+            'item_code': plan.item_code
+        }, ['purchase_uom', 'stock_uom'])
+
+        if not purchase_uom:
+            # assume purchase_uom is stock_uom
+            purchase_uom = stock_uom
+        
+        # Fetch the conversion factor from the purchase_uom
+        conversion = frappe.db.get_value('UOM Conversion Detail', {
+            'parent': plan.item_code, 
+            'uom': purchase_uom
+        }, 'conversion_factor') or 1.0
+        
+        try:
+            actual_qty, ordered_qty = frappe.db.get_value('Bin', {
+                'warehouse': plan.warehouse,
+                'item_code': plan.item_code
+            }, ['actual_qty', 'ordered_qty'])
+        except ValueError as e:
+            # This happen, when the item have no registry in stock yet
+            # no stock transaction, no transfer, no receive, nothing
+            # probably this is a fresh item
+            actual_qty, ordered_qty = 0, 0
+
+        # We compute how many in the next days
+        qty = (plan.sales_velocity + plan.coverage_days) * plan.lead_time_in_days
+        
+        # we reduce the actual stock and the
+        #  items on transit (ordered but not received yet)
+        qty -= (actual_qty + ordered_qty)
+
+        if qty < 0:
+            # We have enought stock, so nothing to do here
+            continue
+
+        # Verify if PO already have the item
+        if (d := doc.get('items', {'item_code': plan.item_code})):
+            d = d[0]
+        else:
+            # Othewise create a new line
+            d = doc.append('items', {'item_code': plan.item_code})
+        
+        # Set the item values to initiate the transaction
+        d.update({
+            'qty': qty,
+            'warehouse': plan.warehouse,
+            'uom': purchase_uom,
+            'stock_uom': stock_uom,
+            'conversion_factor': conversion
+        })
+
+    # Force ERPNext to fetch all missing information on PO and Items
+    doc.run_method('set_missing_values')
+    
+    # Force ERPNext to recalculate taxes and totals
+    doc.run_method('calculate_taxes_and_totals')
+    
+    # TODO: This should be aways avoided
+    # -- append_items should respond to a "GET"
+    # the doc should be returned to screen and synced
+    # save only should happen, when the data is trustable
+    doc.save()
