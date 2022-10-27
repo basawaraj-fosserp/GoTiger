6497c339e99179baa698100eeb864b407464c177
basawaraj-fosserp <103717083+basawaraj-fosserp@users.noreply.github.com>
2022-10-27 12:46:36 +0530

    Update purchase_order.py

--- gotiger/gotiger/doctypes_triggers/purchase_order/purchase_order.py
+++ gotiger/gotiger/doctypes_triggers/purchase_order/purchase_order.py
@@ -43,7 +43,7 @@ def append_items(supplier, name):
             actual_qty, ordered_qty = 0, 0
 
         # We compute how many in the next days
-        qty = (plan.lead_time_in_days + plan.coverage_days) * plan.sales_velocity
+        qty = (plan.lead_time_in_days + plan.coverage_days) * plan.sales_velocity_float
         
         # we reduce the actual stock and the
         #  items on transit (ordered but not received yet)
