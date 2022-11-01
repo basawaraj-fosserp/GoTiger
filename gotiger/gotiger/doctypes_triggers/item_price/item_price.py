030b1505f1910496ce73cd94f12e9b4740c2db6e
Maxwell Morais <MaxMorais@users.noreply.github.com>
2022-10-31 21:02:11 -0300

    Update item_price.py

--- gotiger/gotiger/doctypes_triggers/item_price/item_price.py
+++ gotiger/gotiger/doctypes_triggers/item_price/item_price.py
@@ -1,15 +1,34 @@
 import frappe
 from frappe import _
-from frappe.utils import today
+from frappe.utils import today, add_to_date
 
 def validate(doc,method):
-    itemprice = frappe.db.get_value('Item', {'name': doc.item_code}, 'item_code')
-    item_doc = frappe.get_doc('Item', itemprice)
-    price_item = [c.period_rate for c in item_doc.item_price]
-    if doc.price_list_rate not in price_item:
-        item_doc.append('item_price',{
-            'from_date' :doc.valid_from,
-            'to_date': frappe.utils.today(),
-            'period_rate':doc.price_list_rate
-        })
-    item_doc.save()
+    # When is a new document, just ignore
+    if doc.is_new():
+        return
+    
+    # Fetch previous rate and validity start
+    old_rate, old_valid_from = frappe.db.get_value(doc.doctype, doc.name, ['price_list_rate', 'valid_from'])
+    
+    if old_rate != doc.rate:
+        _today = today()
+        
+        # Ensure validity on actual document
+        doc.valid_from = _today
+        doc.valid_upto = None
+        
+        # Insert an historical price
+        frappe.new_doc('Item Price').update({
+            'item_code': doc.item_code,
+            'uom': doc.uom,
+            'packing_unit': doc.packing_unit,
+            'item_name': doc.item_name,
+            'item_description': doc.item_description,
+            'price_list': doc.price_list,
+            'buying': doc.buying,
+            'selling': doc.selling,
+            'currency': doc.currency,
+            'price_list_rate': old_rate,
+            'valid_from': old_valid_from,
+            'valid_upto': add_to_date(_today, days=-1)
+        }).insert()
